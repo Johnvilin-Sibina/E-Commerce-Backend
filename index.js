a0f25dae183389c3a870394fb1fa3e787022a42b
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-12-22 12:48:13 +0530

    User Registration completed.

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./Database/config.js";
+import authRoute from './Routers/authRouter.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+//Error Handler - middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
+
+//API routes
+app.use('/api/auth',authRoute)
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
