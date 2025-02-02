9df4920f4389a26a42b8cf42464a64fef4684560
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-02-02 23:22:51 +0530

    MongoDB connection issue fixed.

--- Database/config.js
+++ Database/config.js
@@ -7,7 +7,7 @@ const mongoDB_URL = process.env.MONGODB_URL;
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.mongoDB_URL);
+    const connection = await mongoose.connect(mongoDB_URL);
     return connection;
   } catch (error) {
     throw new Error("MongoDB Connection Error.");
