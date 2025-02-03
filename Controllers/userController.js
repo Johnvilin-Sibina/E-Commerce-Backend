50b8ce93da4a4b8e5e5c7c9d85dcf8038a312301
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-02-03 06:44:11 +0530

    Payment success and cancel page url corrected.

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -264,8 +264,8 @@ export const checkoutSession = async (req, res, next) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "https://loot-mart.netlify.app//paymentsuccess",
-      cancel_url: "https://loot-mart.netlify.app//paymentfailure",
+      success_url: "https://loot-mart.netlify.app/paymentsuccess",
+      cancel_url: "https://loot-mart.netlify.app/paymentfailure",
       client_reference_id: user._id,
       expand: ["line_items"],
     });
